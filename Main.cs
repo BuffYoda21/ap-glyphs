eee6ad27a7190ec5c7203b528838851e2946e87e
Hayden Crookham <buffyoda21@gmail.com>
2025-12-11 14:37:40 -0600

    Initial Commit

---
+++ Main.cs
@@ -0,0 +1,19 @@
+using MelonLoader;
+using Il2CppInterop.Runtime.Injection;
+
+[assembly: MelonInfo(typeof(ApGlyphs.Main), "CustomAssetTestMod", "1.0.0", "BuffYoda21")]
+[assembly: MelonGame("Vortex Bros.", "GLYPHS")]
+
+namespace ApGlyphs {
+    public class Main : MelonMod {
+        [System.Obsolete]
+        public override void OnApplicationStart() {
+            if (isInitialized) return;
+            var harmony = new HarmonyLib.Harmony("ApGlyphs.Patches");
+            harmony.PatchAll();
+            // class injection here
+            isInitialized = true;
+        }
+        bool isInitialized = false;
+    }
+}
