e0c2520c87ba22237c501695a724adb95917838c
Hayden Crookham <buffyoda21@gmail.com>
2025-12-12 16:44:49 -0600

    added debug methods

--- NetworkClient.cs
+++ NetworkClient.cs
@@ -19,13 +19,22 @@ namespace ApGlyphs {
                     return;
                 } else {
                     MelonLogger.Msg("Connected to Multiworld server");
+                    MelonLogger.Msg("-------------------------------------------------------------------------");
+                    MelonLogger.Msg("Session Info:");
+                    MelonLogger.Msg("  Game: " + session.ConnectionInfo.Game);
+                    MelonLogger.Msg("  ItemsHandling: " + session.ConnectionInfo.ItemsHandlingFlags);
+                    MelonLogger.Msg("  Slot: " + session.ConnectionInfo.Slot);
+                    MelonLogger.Msg("  Tags: " + string.Join(", ", session.ConnectionInfo.Tags));
+                    MelonLogger.Msg("  Team: " + session.ConnectionInfo.Team);
+                    MelonLogger.Msg("  Uuid: " + session.ConnectionInfo.Uuid);
+                    MelonLogger.Msg("-------------------------------------------------------------------------");
                     isConnected = true;
                 }
             }
         }
 
         public bool Connect() {
-            var session = ArchipelagoSessionFactory.CreateSession(WebHostUrl, WebHostPort);
+            session = ArchipelagoSessionFactory.CreateSession(WebHostUrl, WebHostPort);
             LoginResult loginResult = session.TryConnectAndLogin("GLYPHS",
                                                             SlotName,
                                                             ItemsHandlingFlags.AllItems,
@@ -47,9 +56,30 @@ namespace ApGlyphs {
             }
         }
 
+        public void DEBUG_get_roomstate() {
+            MelonLogger.Msg("DEBUG: " + session.RoomState.ToString());
+        }
+
+        public void DEBUG_get_unchecked_locations() {
+            MelonLogger.Msg("DEBUG:");
+            MelonLogger.Msg("Unchecked locations:");
+            foreach (var location in session.Locations.AllMissingLocations) {
+                MelonLogger.Msg(session.Locations.GetLocationNameFromId(location, null));
+            }
+        }
+
+        public void DEBUG_get_checked_locations() {
+            MelonLogger.Msg("DEBUG:");
+            MelonLogger.Msg("Checked locations:");
+            foreach (var location in session.Locations.AllLocationsChecked) {
+                MelonLogger.Msg(session.Locations.GetLocationNameFromId(location, null));
+            }
+        }
+
         public bool initialized = false;
         public bool isConnected = false;
         private float lastConnectAttempt = -15f;
+        public ArchipelagoSession session;
         private readonly Version ArchipelagoProtocolVersion = new Version(0, 6, 0);
         public string WebHostUrl = "archipelago.gg";
         public int WebHostPort = 0;
