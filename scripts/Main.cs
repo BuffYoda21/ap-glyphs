e39512cd4096f0d804186d38202e4c4db564a36b
BuffYoda21 <buffyoda21@gmail.com>
2026-08-11 13:42:22 -0500

    Bump version number

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using HarmonyLib;
 using UnityEngine.SceneManagement;
 
-[assembly: MelonInfo(typeof(ApGlyphs.Main), "ApGlyphs", "1.1.3-Beta", "BuffYoda21")]
+[assembly: MelonInfo(typeof(ApGlyphs.Main), "ApGlyphs", "1.2.0", "BuffYoda21")]
 [assembly: MelonGame("Vortex Bros.", "GLYPHS")]
 
 namespace ApGlyphs {
